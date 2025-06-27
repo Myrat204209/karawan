1f1427e38514b643fbb3eb156cdfe94df1d32dee
Myratgeldi Annamuhammedow <138607217+Myrat204209@users.noreply.github.com>
2025-06-27 12:04:34 +0500

    feat(house_selection): implement house selection feature with initial UI components

---
+++ lib/features/house_selection/ui/view/house_selection_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:ojak/features/features.dart';
+
+class HouseSelectionPage extends StatelessWidget {
+  const HouseSelectionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return HouseSelectionView();
+  }
+}
---
+++ lib/features/house_selection/ui/view/house_selection_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HouseSelectionView extends StatelessWidget {
+  const HouseSelectionView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
