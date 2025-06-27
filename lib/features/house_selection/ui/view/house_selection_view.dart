0be2dbb2d3e10fdecec4a834d86cdad7e57ff5f2
Myratgeldi Annamuhammedow <138607217+Myrat204209@users.noreply.github.com>
2025-06-27 14:33:43 +0500

    feat: Add support for multiple platforms in migration metadata
    refactor: Simplify AndroidManifest.xml structure and remove unused elements
    
    refactor: Remove MainActivity.kt and associated splash images
    
    refactor: Update styles for Android launch themes and remove deprecated files
    
    feat: Implement HouseSelectionView with PropertyCard widget
    
    chore: Clean up pubspec.yaml and pubspec.lock by removing unused dependencies
    
    feat: Create PropertyCard widget to display property information

--- lib/features/house_selection/ui/view/house_selection_view.dart
+++ lib/features/house_selection/ui/view/house_selection_view.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:ojak/features/features.dart';
 
 class HouseSelectionView extends StatelessWidget {
   const HouseSelectionView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        PropertyCard(),
+        PropertyCard()
+      ],
+    );
   }
 }
--- lib/features/house_selection/ui/widgets/widgets.dart
+++ lib/features/house_selection/ui/widgets/widgets.dart
@@ -1,1 +1,1 @@
-// TODO Implement this library.
+export 'property_card.dart';
