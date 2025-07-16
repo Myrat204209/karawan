9c95eb2f0de85b1e1eb680e14b45fee88aa50d35
Myratgeldi Annamuhammedow <138607217+Myrat204209@users.noreply.github.com>
2025-07-17 01:51:46 +0500

    feat: add login bloc
    - Created login bloc files
    - Updated dependencies
    - Updated pubspec.yaml
    - Improved code style

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -6,19 +6,18 @@ import 'package:get_it/get_it.dart';
 // import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:karawan/app/app.dart';
 import 'package:karawan/main/bootstrap/bootstrap.dart';
-// import 'package:path_provider/path_provider.dart';
+// import 'package:path_provider/path_providf er.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 
 final getIt = GetIt.instance;
-
-Future<void> main() async {
+ Future<void> main() async {
   final talker = TalkerFlutter.init();
   getIt.registerSingleton<Talker>(talker);
   final router = AppRouter();
   getIt.registerSingleton<AppRouter>(router);
 
   bootStrap(talker, () async {
-    // const defaultBaseUrl = 'http://172.16.25.24:8080';
+    // const defaultBaseUrl = 'baseurl:8080';
     // final httpClient = Http(defaultBaseUrl: defaultBaseUrl);
 
     // HydratedBloc.storage = await HydratedStorage.build(
