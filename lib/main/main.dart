4b17b158e2832b42c06b76c26810852dcc0c85e8
Myratgeldi Annamuhammedow <138607217+Myrat204209@users.noreply.github.com>
2025-08-06 22:47:21 +0500

    fix(main): remove duplicate import of storage_provider
    - Removed redundant import of storage_provider from main.dart
    - Cleaned up code for better readability

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:app_ui/src/storage/storage_provider.dart';
 import 'package:data_provider/data_provider.dart';
 import 'package:fast_cached_network_image/fast_cached_network_image.dart';
 import 'package:get_it/get_it.dart';
@@ -7,7 +8,6 @@ import 'package:hive_ce_flutter/hive_flutter.dart';
 // import 'package:hive_ce_flutter/hive_flutter.dart';
 // import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:karawan/app/app.dart';
-import 'package:app_ui/src/storage/storage_provider.dart';
 import 'package:karawan/main/bootstrap/bootstrap.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 // import 'package:path_provider/path_provider.dart';
